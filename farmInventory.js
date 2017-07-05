f55bbd3d843fd2d1c300027cc334fe0cf661202c
Shuvayan Das <shuvayan.das@gmail.com>
2017-07-05 16:35:31 +0530

    Add files via upload

--- farmInventory.js
+++ farmInventory.js
@@ -1,11 +1,13 @@
-function printFarmInventory(cows,chickens){
-	var cowString = String(cows);
-	while (cowString.length < 3)
-		cowString = "0" + cowString;
-	console.log(cowString + "Cows");
-	var chickenString = String(chickens);
-	while (chickenString.length < 3)
-		chickenString = "0" + chickenString;
-	console.log(chickenString + "Chickens");
+function zeroPad(number, width) {
+  var string = String(number);
+  while (string.length < width)
+    string = "0" + string;
+  return string;
 }
-//printFarmInventory(7,11)
+
+function printFarmInventory(cows, chickens, pigs) {
+  console.log(zeroPad(cows, 3) + " Cows");
+  console.log(zeroPad(chickens, 3) + " Chickens");
+  console.log(zeroPad(pigs, 3) + " Pigs");
+}
+//printFarmInventory(7,11,13)
